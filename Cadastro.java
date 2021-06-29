040dd10d647a7d41541b4d698538526b1147f22f
enzolins <enzolins@hotmail.com>
2021-06-29 17:26:58 -0300

    Update Cadastro.java

--- Cadastro.java
+++ Cadastro.java
@@ -1,10 +1,15 @@
+
+
+
 import java.awt.Font;
 import java.awt.event.*;
 import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -21,20 +26,26 @@ public class Cadastro implements ActionListener {
    private JTextField text = new JTextField();
    private JTextField text2 = new JTextField();
    private JTextField text3 = new JTextField();
+   private String[] opcoes = new String[]{"Hardware","Software","Periferico"};
+   private JComboBox<String> comboBox = new JComboBox<>(opcoes);
+   private JLabel label1 = new JLabel();
+   private JLabel label2 = new JLabel();
+   private JLabel label3 = new JLabel();
+   private JLabel label4 = new JLabel();
+   private JLabel label5 = new JLabel();
    private JTextArea textArea = new JTextArea();
    private JScrollPane scroll = new JScrollPane();
    private JMenuBar menuBar = new JMenuBar();
    private JMenu fileMenu = new JMenu("Arquivo");
-   private JMenu editMenu = new JMenu("Editar");
-   private JMenu helpMenu = new JMenu("Ajuda");
+   private JMenu programMenu = new JMenu("Programa");
+   private JMenuItem backItem = new JMenuItem("Voltar");
+   private JMenuItem exiItem = new JMenuItem("Sair");
    private JMenuItem loadItem = new JMenuItem("Carregar");
    private JMenuItem saveItem = new JMenuItem("Salvar");
-   private JMenuItem exitItem = new JMenuItem("Sair");
    private Produtos produto = MenuPrincipal.retornarProduto();
-   private int i;
+   private ListarProdutos lista = new ListarProdutos();
 
     Cadastro(){
-        i = produto.getSize();
 
         //ICONS
         ImageIcon logoIcon = new ImageIcon("EG Logo Official 2000.png");
@@ -44,74 +55,111 @@ public class Cadastro implements ActionListener {
         frame.setJMenuBar(menuBar);
 
         menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(helpMenu);
+        menuBar.add(programMenu);
         menuBar.setFocusable(false);
 
         fileMenu.add(loadItem);
         fileMenu.add(saveItem);
-        fileMenu.add(exitItem);
 
+        programMenu.add(backItem);
+        programMenu.add(exiItem);
+        
         loadItem.addActionListener(this);
         saveItem.addActionListener(this);
-        exitItem.addActionListener(this);
+        backItem.addActionListener(this);
+        exiItem.addActionListener(this);
 
         loadItem.setMnemonic(KeyEvent.VK_C);
         saveItem.setMnemonic(KeyEvent.VK_S);
-        exitItem.setMnemonic(KeyEvent.VK_A);
+        backItem.setMnemonic(KeyEvent.VK_V);
+        exiItem.setMnemonic(KeyEvent.VK_S);
 
         fileMenu.setMnemonic(KeyEvent.VK_A);
-        editMenu.setMnemonic(KeyEvent.VK_E);
-        helpMenu.setMnemonic(KeyEvent.VK_J);
+        programMenu.setMnemonic(KeyEvent.VK_P);
         
 
         //BUTTONS
-        button.setBounds(10, 280, 113, 49);
+        button.setBounds(195, 300, 113, 49);
         button.setText("Cadastrar");
         button.setFont(new Font(null,Font.BOLD,15));
         button.addActionListener(this);
         button.setFocusable(false);
 
-        button2.setBounds(10, 389, 113, 49);
+        button2.setBounds(10, 400, 113, 49);
         button2.setText("Listar");
         button2.setFont(new Font(null,Font.BOLD,15));
         button2.addActionListener(this);
         button2.setFocusable(false);
         
         //TEXT FIELD 1
-        text.setBounds(133, 280, 175, 49);
+        text.setBounds(170, 60, 175, 49);
         text.setFont(new Font("Consolas",Font.PLAIN,35));
         text.setForeground(Color.BLACK);
         text.setBackground(Color.WHITE);
         text.setCaretColor(Color.BLACK);
 
          //TEXT FIELD 2
-        text2.setBounds(318, 280, 175, 49);
+        text2.setBounds(170, 120, 175, 49);
         text2.setFont(new Font("Consolas",Font.PLAIN,35));
         text2.setForeground(Color.BLACK);
         text2.setBackground(Color.WHITE);
         text2.setCaretColor(Color.BLACK);
 
-        //TEXT FIELD 2
-        text3.setBounds(503, 280, 175, 49);
+        //TEXT FIELD 3
+        text3.setBounds(170, 180, 175, 49);
         text3.setFont(new Font("Consolas",Font.PLAIN,35));
         text3.setForeground(Color.BLACK);
         text3.setBackground(Color.WHITE);
         text3.setCaretColor(Color.BLACK);
 
+        //COMBO BOX
+        comboBox.setBounds(170, 240, 175, 49);
+        comboBox.setFont(new Font("Consolas",Font.PLAIN,25));
+        comboBox.setForeground(Color.BLACK);
+        comboBox.addActionListener(this);
+        
+        //LABEL 1
+        label1.setText("Produto");
+        label1.setForeground(Color.black);
+        label1.setFont(new Font("Consolas",Font.PLAIN,25));
+        label1.setBounds(10, 60, 150, 49);
+
+        //LABEL 2
+        label2.setText("Valor");
+        label2.setForeground(Color.black);
+        label2.setFont(new Font("Consolas",Font.PLAIN,25));
+        label2.setBounds(10, 120, 150, 49);
+
+        //LABEL 3
+        label3.setText("Quantidade");
+        label3.setForeground(Color.black);
+        label3.setFont(new Font("Consolas",Font.PLAIN,25));
+        label3.setBounds(10, 180, 150, 49);      
+
+        //LABEL 4
+        label4.setText("Categoria");
+        label4.setForeground(Color.black);
+        label4.setFont(new Font("Consolas",Font.PLAIN,25));
+        label4.setBounds(10, 240, 150, 49);     
+
+        //LABEL 5
+        label5.setText("CADASTRO");
+        label5.setForeground(Color.black);
+        label5.setFont(new Font("Consolas",Font.BOLD,35));
+        label5.setBounds(351, 11, 175, 49);
+
         //TEXT AREA
-        //textArea.setBounds(133, 389, 741, 200);
         textArea.setForeground(Color.BLACK);
         textArea.setFont(new Font("Consolas",Font.PLAIN,20));
-       // textArea.setVisible(false);
+        textArea.setEditable(false);
+       
         
         
         //SCROLL PANE
         scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scroll.setViewportView(textArea);
-        scroll.setVisible(false);
-        scroll.setBounds(148, 389, 726, 200);
+        scroll.setBounds(148, 400, 726, 200);
         
 
         //FRAME
@@ -128,6 +176,12 @@ public class Cadastro implements ActionListener {
         frame.add(text);
         frame.add(text2);
         frame.add(text3);
+        frame.add(comboBox);
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(label4);
+        frame.add(label5);
         frame.add(scroll);
     }
 
@@ -137,33 +191,38 @@ public class Cadastro implements ActionListener {
             String nome = text.getText();
             double valor = Double.parseDouble(text2.getText());
             int quantidade = Integer.parseInt(text3.getText());
-            produto.cadastrarProduto(nome, valor, quantidade);
-            i = produto.getSize();
+            String categoria = comboBox.getSelectedItem().toString();
+            produto.cadastrarProduto(nome, valor, quantidade, categoria);
             text.setText("");
             text2.setText("");
             text3.setText("");
         }
         if(e.getSource()==button2){
             textArea.setText("");
-            textArea.setEditable(true);
-            scroll.setVisible(true);
-            for (int z = 0; z<i; z++){
-                textArea.append("Produto: " + produto.getNome(z) + "   " 
-                + "Preco: R$" + produto.getPreco(z) + "   " 
-                + "Em estoque: " + produto.getQuantidade(z) + "\n");
-            }
-            textArea.setEditable(false);
+            lista.listarTodos();
+            textArea.append(lista.getSaida());
         }
         if(e.getSource()==loadItem){
             System.out.println("CARREGANDO!!!");
         }
         if(e.getSource()==saveItem){
             System.out.println("SALVANDO!!!");
+            try {
+                Salvar.salvarProdutos();
+                Salvar.salvarPreco();
+                Salvar.salvarQuantidade();
+                Salvar.salvarCategoria();
+            } catch (Exception q) {
+                q.printStackTrace();
+            }
         }
-        if(e.getSource()==exitItem){
+        if(e.getSource()==backItem){
            frame.dispose();
            MenuPrincipal.retornarParaMenu();
         }
+        if(e.getSource()==exiItem){
+            System.exit(0);
+         }
         
     }
 }
