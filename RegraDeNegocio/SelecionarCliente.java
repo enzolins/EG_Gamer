0424f64a7b69d428d1b7bf23f72789647a33732b
GuiTeles29 <65751010+GuiTeles29@users.noreply.github.com>
2021-07-16 16:10:14 -0300

    Atualizando a regra de negocio
    Co-Authored-By: enzolins <85700008+enzolins@users.noreply.github.com>

--- RegraDeNegocio/SelecionarCliente.java
+++ RegraDeNegocio/SelecionarCliente.java
@@ -3,16 +3,13 @@ import InterfaceGrafica.MenuPrincipal;
 
 public class SelecionarCliente {
     private static Cliente cliente = MenuPrincipal.retornarCliente();
-    private static int id=0;
+    private static int id;
     
     public static int retornarIdDoCliente(String nome){
-        
+        id=0;
         while(!cliente.getNome(id).equals(nome)){
             id += 1;
         }
-
         return id; 
     }
-
-
 }
